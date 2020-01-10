bc799c69642a852023f2d40681e526fb2c0d2567
j3ffu633zy <jeffubayi@gmail.com>
2020-01-10 12:30:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+###Auto Tweet
+
+A python script to automate the process of posting tweets
+
+##Libraries and Requirements
+Tweepy - library for twitter API
+Pysocks - send traffic throughs  and HTTP proxy servers
+Certifi -verification of trustworthyness in secure shell 
+idna- internaltionalized domain name application
+Six -is a Python 2 and 3 compatibility library
+
+##How to Run the Script
+
+     #install dependencies
+                  py -m venv env
+                  env\Scripts\activate
+                  pip install -r requirements.txt
+
+      #Add a credentials.py in the follwing format 
+Link to creating tweepy [credentials](https://developer.twitter.com)
+
+                      ACCESS_TOKEN = "####"
+                      ACCESS_TOKEN_SECRET = "####"
+                      CONSUMER_KEY = "####"
+                      CONSUMER_SECRET = "####"
+
+     #Adjust the NUM_OF_TWEETS and WAIT_TIME parameter in  script.py. Make sure to not have a very small WAIT_TIME.
+
+    #Run the script
+                        python script.py
+
---
+++ script.py
@@ -0,0 +1,45 @@
+import credentials
+import tweepy
+from tweepy.streaming import Stream, StreamListener
+import time
+
+from random import randint
+import string
+import random
+
+NUM_OF_TWEETS = 1
+
+
+class TweetAutomation:
+    consumer_key = credentials.CONSUMER_KEY
+    consumer_secret = credentials.CONSUMER_SECRET
+    access_token = credentials.ACCESS_TOKEN
+    access_token_secret = credentials.ACCESS_TOKEN_SECRET
+
+    api = tweepy.API()
+
+    WAIT_TIME = 15
+    HASH_TAG = " #100DaysOfCode"
+    messages = [" Hi guys, this is a python script to automate  process of posting tweet. Notice the 'Twitter for' is ranged to my credentials. "]
+
+    def __init__(self):
+        self.auth()
+
+    def auth(self):
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(auth)
+
+    def send_tweet(self):
+        idx = randint(0, len(self.messages) - 1)
+        random_str = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        self.api.update_status(status=self.messages[idx] + " \n"  + str(random_str) + self.HASH_TAG)
+        time.sleep(self.WAIT_TIME)
+
+
+if __name__ == "__main__":
+    tweet_automation = TweetAutomation()
+    for i in range(NUM_OF_TWEETS):
+        tweet_automation.send_tweet()
+    
